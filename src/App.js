448b6dc9bc862752ce1867d4ca009f064c100e18
pooja sankhala <poojasankhala97@gmail.com>
2022-07-25 18:27:06 +0530

    Basic todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [toggleBtn, setToggleBtn] = useState(true);
+  const [edit, setEdit] = useState(null);
+  const addTodo = () => {
+    if (!input) {
+      alert("Please Enter Your Task");
+    } else if (input && !toggleBtn) {
+      setTodos(
+        todos.map((e) => {
+          if (e.id === edit) {
+            return { ...e, task: input };
+          }
+          return e;
+        })
+      );
+      setToggleBtn(true);
+      setInput("");
+      setEdit(null);
+    } else {
+      const newTodos = {
+        id: Math.floor(Math.random() * 1000),
+        task: input,
+      };
+      setTodos([...todos, newTodos]);
+      setInput("");
+      // console.log(todos);
+    }
+  };
+
+  const handleDelete = (id) => {
+    const del = todos.filter((todo) => todo.id !== id);
+    setTodos(del);
+  };
+
+  const handleEdit = (id) => {
+    const edit = todos.find((todo) => {
+      return todo.id === id;
+    });
+    console.log(edit);
+    setToggleBtn(false);
+    setInput(edit.task);
+    setEdit(id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Enter Your Task..."
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      {toggleBtn ? (
+        <button onClick={addTodo}>Add Task</button>
+      ) : (
+        <button onClick={addTodo}>Edit</button>
+      )}
+
+      <div>
+        {todos.map((todo) => {
+          return (
+            <ul key={todo.id}>
+              <li>{todo.task}</li>
+              <button onClick={() => handleEdit(todo.id)}>Edit</button>
+              <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
